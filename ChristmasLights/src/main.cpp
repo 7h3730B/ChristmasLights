f9c312c51c909e52794a5fb22a4e4681d84be396
7h3730B <56200898+7h3730B@users.noreply.github.com>
2020-12-29 00:07:23 +0100

    Works now like a charm (I hope)

--- ChristmasLights/src/main.cpp
+++ ChristmasLights/src/main.cpp
@@ -1,11 +1,13 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
 #include <IRremoteESP8266.h>
 #include <IRsend.h>
+#include <FS.h>
 
-const char *ssid = "Wlan-Repeater-Schulze";
-const char *password = "1cVcMBsgUt2ifR5lTdpXe6Td3";
+const char *ssid = "YOUR-WIFI-SSID-GOES-HERE";
+const char *password = "YOUR-WIFI-PASSWORD-GOES-HERE";
 
 ESP8266WebServer server(80);
 
@@ -15,6 +17,8 @@ IRsend irsend(kIrLed);
 
 boolean connectWifi();
 void handleNotFound();
+String getContentType(String filename);
+boolean handleFileRead(String path);
 
 void setup() {
     Serial.begin(115200);
@@ -22,16 +26,30 @@ void setup() {
     
     irsend.begin();
 
+    SPIFFS.begin();
+
     server.on("/api", [](){
         server.send(200, "text/plain", "OK");
         uint16_t raw_data[20];
         for (int i = 0; i < server.args(); i++) {
             raw_data[i] = server.arg(i).toInt();
         }
-        irsend.sendRaw(raw_data, 19, 35);
+        for (int i = 0; i <= 6; i++) {
+            irsend.sendRaw(raw_data, 19, 35);
+        }
     });
-    server.onNotFound(handleNotFound);
+    server.onNotFound([]() {
+        if (!handleFileRead(server.uri())) {
+            handleNotFound();
+        }
+    });
     server.begin();
+
+    if (MDNS.begin("SuperDuperCooleLights")) {
+        Serial.println("mDNS responder started");
+    } else {
+        Serial.println("Error setting up mDNS responder");
+    }
 }
 
 void loop() {
@@ -63,4 +81,24 @@ void handleNotFound(){
     message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
   }
   server.send(404, "text/plain", message);
+}
+
+String getContentType(String filename) {
+    if (filename.endsWith(".html")) return "text/html";
+    else if (filename.endsWith(".css")) return "text/css";
+    else if (filename.endsWith(".js")) return "application/javascript";
+    else if (filename.endsWith(".ico")) return "image/x-icon";
+    return "text/plain";
+}
+
+boolean handleFileRead(String path) {
+    if (path.endsWith("/")) path += "index.html";
+    String contenttype = getContentType(path);
+    if (SPIFFS.exists(path)) {
+        File file = SPIFFS.open(path, "r");
+        size_t sent = server.streamFile(file, contenttype);
+        file.close();
+        return true;
+    }
+    return false;
 }
--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # ChristmasLights
 Controlling our Christmastress Lights over the Web with ESP8266
-TODO:
-Send the IR 
+![Website Screenshot](Website.png)
